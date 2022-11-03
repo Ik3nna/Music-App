7405ee1dd54b0e6f90ea31b625574fb05c739db3
Ik3nna <iamiykendukwu@gmail.com>
2022-11-03 18:03:41 +0100

    header

---
+++ src/components/header/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header () {
+
+    return(
+        <header>
+            <div className="overlay"></div>
+            <img className="menu" src="/assets/menu 1.svg" alt="menu" />
+
+            <img src="/assets/logo.svg" alt="logo" />
+
+            <div>
+                <img src="/assets/search.svg" alt="search" />
+                <input type="text" placeholder="Search artists" />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
