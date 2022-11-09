162cd8d70c40418f6bf8623b97eef6e27be1646e
Ik3nna <iamiykendukwu@gmail.com>
2022-11-09 12:57:52 +0100

    Fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,29 @@
 import React from "react";
+import useSticky from "./utils";
 
 function Header () {
+    const { sticky, stickyRef } = useSticky();
 
     return(
-        <header>
-            <div className="head">
-                <div className="overlay"></div>
-                <img className="menu" src="/assets/menu 1.svg" alt="menu" />
+        <>
+            <header ref={stickyRef} className={`${sticky && "fixed-nav"}`}>
+                <div className="head">
+                    <div className="overlay"></div>
+                    <img className="menu" src="/assets/menu 1.svg" alt="menu" />
 
-                <img src="/assets/logo.svg" alt="logo" />
+                    <img src="/assets/logo.svg" alt="logo" />
 
-                <div>
-                    <img src="/assets/search.svg" alt="search" />
-                    <input type="text" placeholder="Search artists" />
+                    <div className="search">
+                        <img src="/assets/search.svg" alt="search" />
+                        <input type="text" placeholder="Search artists" />
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            <div style={{
+            height: sticky ? `${stickyRef.current?.clientHeight}px` : '0px',
+            }} />
+        </>
     );
 }
 
