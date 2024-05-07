94a603060d9de7b1a4b21e14bd3c86135bc5baa9
ik3nna <iamiykendukwu@gmail.com>
2024-05-07 14:24:47 +0100

    fix: Loading component fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Home from "./pages/Home/home";
 import Header from "./components/header";
 import Album from "./pages/Albums/album";
@@ -15,38 +15,35 @@ import { AnimatePresence } from "framer-motion";
 function App() {
   const { loading } = useGlobalContext();
 
-  if (loading) {
-    return(
-      <div className="spinner">
-        <MutatingDots 
-          visible={true}
-          height="100"
-          width="100"
-          color="#FACD66"
-        />
-      </div>
-    )
-  }
-
   return (
     <>
-      <BrowserRouter>
-        <Header />
-          
-        <AnimatePresence onExitComplete>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/collections" element={<Collection />} />
-            <Route path="/album/:id" element={<Album />} />
-            <Route path="/radio" element={<Radio />} />
-            <Route path="/video" element={<Video />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/" element={<Navigate replace to="/home" />} />
-          </Routes>
-        </AnimatePresence>
-          
-        <PlayMusic />
-      </BrowserRouter>
+      {loading ?
+        <div className="spinner">
+          <MutatingDots 
+            visible={true}
+            height="100"
+            width="100"
+            color="#FACD66"
+          />
+        </div> :
+        <BrowserRouter>
+          <Header />
+            
+          <AnimatePresence onExitComplete>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/collections" element={<Collection />} />
+              <Route path="/album/:id" element={<Album />} />
+              <Route path="/radio" element={<Radio />} />
+              <Route path="/video" element={<Video />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/" element={<Navigate replace to="/home" />} />
+            </Routes>
+          </AnimatePresence>
+            
+          <PlayMusic />
+        </BrowserRouter>
+      }
     </>  
   );
 }
